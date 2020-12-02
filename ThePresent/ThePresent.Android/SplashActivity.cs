b1dc0669fe1b475c6de8267c28a962c441f4e425
Aleksandr Andreev <alivemenone@gmail.com>
2020-12-02 16:07:24 +0300

    Add project files.

---
+++ ThePresent/ThePresent.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace ThePresent.Droid
+{
+    [Activity(Label = "ThePresent", Icon = "@mipmap/icon", NoHistory = true, Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
